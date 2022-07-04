aabd41e3da2ac1791e5f6431203e28aea762213f
locth-1708 <49936698+locth-1708@users.noreply.github.com>
2022-07-04 22:15:05 +0700

    solution bai 10

--- session10/solution.js
+++ session10/solution.js
@@ -12,11 +12,148 @@
 // select option chọn lớp: FED01 đến FED03
 // có nút submit và nút reset, tất cả các trường đều required
 
+const $ = (selector) => document.querySelector(selector);
+const $All = (selector) => document.querySelectorAll(selector);
+
+const validationRule = {
+  email: {
+    min: 0,
+    max: 50,
+    isRequired: true,
+    regExPattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  },
+  birthday: {
+    min: 10,
+    max: 10,
+    isRequired: true,
+    regExPattern: '',
+  },
+  phoneNumber: {
+    min: 10,
+    max: 12,
+    isRequired: true,
+    regExPattern: /(84|0[3|5|7|8|9])+([0-9]{8})\b/g,
+  },
+  genders: {
+    min: 0,
+    max: 50,
+    isRequired: true,
+    regExPattern: '',
+  },
+  courses: {
+    min: 0,
+    max: 50,
+    isRequired: true,
+    regExPattern: '',
+  },
+  classes: {
+    min: 0,
+    max: 50,
+    isRequired: true,
+    regExPattern: '',
+  },
+};
+
+document.getElementById('submit').onclick = () => {
+  const email = $('#email').value;
+  const birthday = $('#birthday').value;
+  const classes = $('#classes').value;
+  const phoneNumber = $('#phoneNumber').value;
+  const genders = [...$All('[name=gender]')]
+    .map((input) => {
+      if (input.checked) return input.value;
+    })
+    .filter((item) => item);
+  const courses = [...$All('[name=course]')]
+    .map((input) => {
+      if (input.checked) return input.value;
+    })
+    .filter((item) => item);
+
+  const fieldValue = {
+    email: email,
+    birthday: birthday,
+    classes: classes,
+    phoneNumber: phoneNumber,
+    genders: genders,
+    courses: courses,
+  };
+
+  const arrayFieldValid = Object.keys(fieldValue).map((keyItem) => {
+    console.log('arrayFieldValid ~ keyItem', keyItem);
+    if (!fieldValue[keyItem] && validationRule[keyItem].isRequired)
+      return { [keyItem]: false };
+
+    if (
+      fieldValue[keyItem].length < validationRule[keyItem].min ||
+      fieldValue[keyItem].length > validationRule[keyItem].max
+    )
+      return { [keyItem]: false };
+
+    if (validationRule[keyItem].regExPattern) {
+      return {
+        [keyItem]: validationRule[keyItem].regExPattern.test(
+          fieldValue[keyItem]
+        ),
+      };
+    }
+
+    return { [keyItem]: true };
+  });
+
+  arrayFieldValid.map((item) => {
+    const keyItem = Object.keys(item)[0];
+    const valueItem = Object.values(item)[0];
+    if (valueItem) {
+      $(`#${keyItem}`).style.color = 'green';
+    } else {
+      $(`#${keyItem}`).style.color = 'red';
+    }
+  });
+};
+
 // Bài 2: (Optional)
 // Tạo ứng dụng đơn xin nghỉ, có các field và rule validation theo yêu cầu:
 // username: ít nhất 8 ký tự, chỉ chấp nhận ký tự, số và dấu _
 // select option chọn team: giá trị từ team1 đến team4
 // Textarea lý do nghỉ (Reason): nhập không quá 100 từ
+
+const now = new Date();
+const dateNow = now.getDate();
+const monthNow = now.getMonth() + 1;
+const yearNow = now.getFullYear();
+
+let dateMin;
+let monthMin;
+let yearMin;
+
+if (dateNow <= 3) {
+  if (monthNow === 1) {
+    yearMin = yearNow - 1;
+    monthMin = 12;
+  } else {
+    monthMin = monthNow - 1;
+    yearMin = yearNow;
+  }
+  dateMin = dateNow - 3 + 30;
+} else {
+  monthMin = monthNow;
+  dateMin = dateNow - 3;
+  yearMin = yearNow;
+}
+
+const minDate = `${yearMin}-${monthMin < 10 ? '0' + monthMin : monthMin}-${
+  dateMin < 10 ? '0' + dateMin : dateMin
+}`;
+console.log('minDate', minDate);
+
+$('#from').setAttribute('min', minDate);
+
+const from = $('#from').value;
+const to = $('#to').value;
+new Date(from).getMilliseconds();
+new Date(to).getMilliseconds();
+
 // Nghỉ từ (Form): có thể chọn ngày quá khứ không quá ngày hiện tại 3 ngày (ví dụ hôm nay là 31/5/2022,
 //  thì có thể chọn tối đa từ ngày 28/05/2022
 // Nghỉ đến (To): Nếu số ngày nghỉ lớn hơn 2 ngày (ví dụ From: 28/05/2022, To: 29/05/2022 => là 2 ngày)
